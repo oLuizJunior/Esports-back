2cc2e2fe91f690d8bb4bd94dcc00739b0742aa9b
oLuizJunior <junior.luiz182@gmail.com>
2022-09-15 17:40:09 -0300

    criando rotas e tabelas do db

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,36 @@
 //const express = require('express');
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
-app.get('/ads', (req, res) => {
+//methods http / API RESTful
+//GET, POST, PUT, PATCH, DELETE
+
+app.get('/games', (req, res) => {
+  return res.json([])
+})
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([])
+})
+
+app.get('/games/:id/ads', (req, res) => {
+  //const gameId = req.params.id;
+
   return res.json([
     {id: 1, anuncio: 'Anuncio 1'},
     {id: 2, anuncio: 'Anuncio 2'},
     {id: 3, anuncio: 'Anuncio 3'},
     {id: 4, anuncio: 'Anuncio 4'},
     {id: 5, anuncio: 'Anuncio 5'},
+  ])
+})
+
+app.get('/ads/:id/discord', (req, res) => {
+  //const adId = req.params.id;
+
+  return res.json([
+    
   ])
 })
 
